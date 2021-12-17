d9891ede37b22c6e455b0ee48289aaff6cee7214
Aayush Basnet <aayushbasnet07@gmail.com>
2021-12-17 21:54:59 +0545

    api and ui updated

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,69 @@
-const key = 'GxbQlc4DWYjwGWkWBap1Ohz8Vw18rdGu';
+const cityForm = document.querySelector('.city_form');
+const card = document.querySelector('.card');
+const city_time = document.querySelector('img.city_time');
+const weather_icon = document.querySelector('.weather_icon img')
+const weather_details = document.querySelector('.weather_details');
 
-const getCity = async (city) =>{
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-    const query = `?apikey=${key}&q=${city}`;
+//update UI
+const updateUi = (data) => {
+    const {cityDetails, weather} = data;
+    weather_details.innerHTML = 
+    `
+        <h5 class="my-3">${cityDetails.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+        </div>
+        <div class="clock my-3 mx-2 float-right">
+            <span>Clock</span>
+        </div>  
+    `;
 
-    const response = await fetch(base + query);
-    const data = await response.json();
-    console.log(data);
-}
+    //update time images
+    const timeSrc = weather.IsDayTime ? 'images/day.svg' : 'images/night.svg';
+    city_time.setAttribute('src', timeSrc);
 
-getCity('manchester');
+    //update icon
+    const iconSrc = `images/icons/${weather.WeatherIcon}.svg`;
+    weather_icon.setAttribute('src', iconSrc);
+
+    //display card
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+
+    //clock
+    setInterval(()=>{
+        
+    },)
+};
+
+//update city UI
+const updateCity = async (city)=>{
+    const cityDetails = await getCity(city);
+    const weather = await getWeather(cityDetails.Key);
+    
+    return {cityDetails, weather};
+};
+
+//Get city form
+cityForm.addEventListener('submit', e => {
+    //prevent default reload
+    e.preventDefault();
+
+    //get city from user
+    const city = cityForm.city.value.trim();
+    //reset form
+    cityForm.reset();
+
+    //update city 
+    updateCity(city).then(data => {
+        console.log(data);
+        updateUi(data);
+    }).catch(err => {
+        console.log(err);
+    });
+    
+});
+
