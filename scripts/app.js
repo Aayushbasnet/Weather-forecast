f01a62f1dcd695efc2e6531a1caa2960f130cab5
Aayush Basnet <aayushbasnet07@gmail.com>
2021-12-21 11:20:47 +0545

    oop concept and clock

--- scripts/app.js
+++ scripts/app.js
@@ -3,14 +3,20 @@ const card = document.querySelector('.card');
 const city_time = document.querySelector('img.city_time');
 const weather_icon = document.querySelector('.weather_icon img')
 const weather_details = document.querySelector('.weather_details');
-// const clock = document.querySelector('.clock');
+const forecast = new Forecast();
 
-// const reg = / T([0-9]{2}\:[0-9]{2}\:[0-9]{2})\+ /g;
-//update UI
 const updateUi = (data) => {
     const {cityDetails, weather} = data;
-    const clock = weather.LocalObservationDateTime;
-    // console.log(clock.match(reg));
+    //to get time from weather api
+    const localDateTime = weather.LocalObservationDateTime;
+    const timeArray = Array.from(localDateTime);
+    var cityTime = "";
+    const clock = timeArray.slice(timeArray.indexOf('T')+1, timeArray.indexOf('+'));
+    const strClock = clock.forEach(clk => {
+        cityTime += clk.toString();
+    });
+    console.log(cityTime);
+    //update ui after getting data
     weather_details.innerHTML = 
     `
         <h5 class="my-3">${cityDetails.EnglishName}</h5>
@@ -20,7 +26,7 @@ const updateUi = (data) => {
         <span>&deg;C</span>
         </div>
         <div class="clock my-3 mx-2 float-right">
-            <span>${weather.LocalObservationDateTime}</span>
+            <span>Time: ${cityTime}</span>
         </div>  
     `;
 
@@ -39,14 +45,6 @@ const updateUi = (data) => {
 
 };
 
-//update city UI
-const updateCity = async (city)=>{
-    const cityDetails = await getCity(city);
-    const weather = await getWeather(cityDetails.Key);
-    
-    return {cityDetails, weather};
-};
-
 //Get city form
 cityForm.addEventListener('submit', e => {
     //prevent default reload
@@ -58,7 +56,7 @@ cityForm.addEventListener('submit', e => {
     cityForm.reset();
 
     //update city 
-    updateCity(city).then(data => {
+    forecast.updateCity(city).then(data => {
         console.log(data);
         updateUi(data);
     }).catch(err => {
@@ -71,7 +69,7 @@ cityForm.addEventListener('submit', e => {
 });
 
 if(localStorage.getItem('city')){
-    updateCity(localStorage.getItem('city'))
+    forecast.updateCity(localStorage.getItem('city'))
         .then(data => {
             updateUi(data);
         })
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,21 +1,31 @@
-const key = 'GxbQlc4DWYjwGWkWBap1Ohz8Vw18rdGu';
+class Forecast{
+    constructor(){
+        this.key = 'GxbQlc4DWYjwGWkWBap1Ohz8Vw18rdGu';
+        this.weatherUrl = 'http://dataservice.accuweather.com/currentconditions/v1/';
+        this.cityUrl = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    }
 
-//get weather api of the city
-const getWeather = async (id) => {
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-    const query = `${id}?apikey=${key}`;
+    // update city
+    async updateCity(city){
+        const cityDetails = await this.getCity(city);
+        const weather = await this.getWeather(cityDetails.Key);
+        return {cityDetails, weather};
+    }
 
-    const response = await fetch(base + query);
-    const data = await response.json();
-    return data[0];
-};
+    //get city api
+    async getCity(city){
+        const query = `?apikey=${this.key}&q=${city}`;
+        const response = await fetch(this.cityUrl + query);
+        const data = await response.json();
+        return data[0];
+    }
 
-//get city api
-const getCity = async (city) =>{
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-    const query = `?apikey=${key}&q=${city}`;
-
-    const response = await fetch(base + query);
-    const data = await response.json();
-    return data[0];
+    //get weather api of that city
+    async getWeather(id){
+        const query = `${id}?apikey=${this.key}`;
+        const response = await fetch(this.weatherUrl + query);
+        const data = await response.json();
+        return data[0];
+    }
 }
+
